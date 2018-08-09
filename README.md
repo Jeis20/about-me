be22d00cabdd825278db0c38ac1ae9c9f293e726
eckermania <erineckerman@gmail.com>
2018-08-09 14:02:22 -0700

    Added function wrapping and added authors to README.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # about-me
 Code 201, Day 2 Project: About Me
+Primary code owner is Joey Eisenzimmer with input on function wrapping from Erin Eckerman.
--- js/app.js
+++ js/app.js
@@ -9,109 +9,124 @@ var correctAnswers = 0;
 
 // Ask them five y/n questions
 
-function (questionCount1){
-    var nums = [1]
-    for (var i = 0; i < num.length; i++){
-    
-var answerOne = prompt('Do I have any pets?').toLowerCase();
+function quesntionCountOne(){
+  var num = [1];
+  for (var i = 0; i < num.length; i++){
+
+    var answerOne = prompt('Do I have any pets?').toLowerCase();
     if (answerOne === 'y'|| answerOne === 'yes') {
-        alert('You got it right!');
-            correctAnswers++;
-        console.log('The' + userName + 'was asked if I have any dogs and the answer is yes.')
-}
-    else {
-        alert('Epic Fail. Of course I do, this is Seattle!');
-        }
+      alert('You got it right!');
+      correctAnswers++;
+      console.log('The' + userName + 'was asked if I have any dogs and the answer is yes.');
     }
-}
-function (questionCount1);
-
-function (questionCount2) {
-var answerTwo = prompt('Do I have any dragons?').toLowerCase();
-    if (answerTwo === 'n'|| answerTwo === 'no') {
-        alert('Correct! Breathe a sigh of relief.. and not fire.');
-            correctAnswers++;
-         console.log('The' + userName + 'was asked if I have any dragons and the answer is no.')
-}
     else {
-        alert('No, but I wish I did!');
-        }
+      alert('Epic Fail. Of course I do, this is Seattle!');
     }
-    function(questionCount2);
-
-var answerThree = prompt('Do I like chai tea lattes?').toLowerCase();
-    if (answerThree === 'n'|| answerThree === 'no') {
-        alert('With all my heart no, you got it.');
-            correctAnswers++
-        console.log('The' + userName + 'was asked if I chai tea latte and the answer is no.')
+  }
 }
-    else {
-        alert('No way jose, I don\'t like it!');
-        }
+quesntionCountOne();
 
-var answerFour = prompt('Do I own a DeLorean?').toLowerCase();
-    if (answerFour === 'n'|| answerFour === 'no') {
-        alert('Right! Sadly, no time travel for me.');
-            correctAnswers++
-         console.log('The' + userName + 'was asked if I own a DeLorean and the answer is no.')
- }
-    else {
-        alert('Whoaaa whoaaa, sorry incorrect!');
-        }      
+function questionCountTwo () {
+  var answerTwo = prompt('Do I have any dragons?').toLowerCase();
+  if (answerTwo === 'n'|| answerTwo === 'no') {
+    alert('Correct! Breathe a sigh of relief.. and not fire.');
+    correctAnswers++;
+    console.log('The' + userName + 'was asked if I have any dragons and the answer is no.');
+  }
+  else {
+    alert('No, but I wish I did!');
+  }
+}
+questionCountTwo();
 
-var answerFive = prompt('Do I have a Denver Broncos jersey for my dog?').toLowerCase();
-    if (answerFive === 'y'|| answerFive === 'yes') {
-        alert('You\'re darn tootin\', correct!');
-            correctAnswers++
-         console.log('The' + userName + 'was asked if my dog has a Denver Broncos jersey and the answer is yes.')
- }
-    else {
-        alert('How could you get this wrong?! I thought you knew me..');
-         }   
+function questionCountThree(){
+  var answerThree = prompt('Do I like chai tea lattes?').toLowerCase();
+  if (answerThree === 'n'|| answerThree === 'no') {
+    alert('With all my heart no, you got it.');
+    correctAnswers++;
+    console.log('The' + userName + 'was asked if I chai tea latte and the answer is no.');
+  }
+  else {
+    alert('No way jose, I don\'t like it!');
+  }
+}
+questionCountThree();
 
-var specialNumber = 84;
-var num = 3;
-while (num > 0) {
+function questionCountFour(){
+  var answerFour = prompt('Do I own a DeLorean?').toLowerCase();
+  if (answerFour === 'n'|| answerFour === 'no') {
+    alert('Right! Sadly, no time travel for me.');
+    correctAnswers++;
+    console.log('The' + userName + 'was asked if I own a DeLorean and the answer is no.');
+  }
+  else {
+    alert('Whoaaa whoaaa, sorry incorrect!');
+  }
+}
+questionCountFour();
+
+function questionCountFive(){
+  var answerFive = prompt('Do I have a Denver Broncos jersey for my dog?').toLowerCase();
+  if (answerFive === 'y'|| answerFive === 'yes') {
+    alert('You\'re darn tootin\', correct!');
+    correctAnswers++;
+    console.log('The' + userName + 'was asked if my dog has a Denver Broncos jersey and the answer is yes.');
+  }
+  else {
+    alert('How could you get this wrong?! I thought you knew me..');
+  }
+}
+questionCountFive();
+
+function questionCountSix(){
+  var specialNumber = 84;
+  var num = 3;
+  while (num > 0) {
     var answerSix = parseInt(prompt('Let\'s play a game, what\'s my favorite number between 0 to 100, and based on multiples of 12?'));
-        console.log('answerSix', answerSix);
+    console.log('answerSix', answerSix);
     // if (num <= 3 && num > 0) {
-        if (answerSix === specialNumber) {
-            alert('Well look at you, you gone and done it. Yeeehawwww!');
-            correctAnswers++
-            break;
-        } else if (answerSix > specialNumber) {
-            alert('Too high, try again!');
-        } else if (answerSix < specialNumber) {
-            alert('Too low, give it another go!');            
-            // var answerSix = prompt('Let\'s play a game, what\'s my favorite number between 0 to 100, and based on multiples of 12?');
-        }
-        num--;
+    if (answerSix === specialNumber) {
+      alert('Well look at you, you gone and done it. Yeeehawwww!');
+      correctAnswers++;
+      break;
+    } else if (answerSix > specialNumber) {
+      alert('Too high, try again!');
+    } else if (answerSix < specialNumber) {
+      alert('Too low, give it another go!');
+      // var answerSix = prompt('Let\'s play a game, what\'s my favorite number between 0 to 100, and based on multiples of 12?');
+    }
+    num--;
     // }
+  }
+  if(num === 0) {
+    alert('Nah nah nah nah, hey hey hey, goooodddbyyyeeee');
+  }
 }
-        if(num === 0) {
-            alert('Nah nah nah nah, hey hey hey, goooodddbyyyeeee');
-    }
+questionCountSix();
 
-    var dogBreeds = ["Chihuahua","German Shepherd","Rottweiler"];
-    var answerSeven;
-    var num = 6;
-    while (num > 0) {
-        answerSeven = prompt('Can you guess any of the different breeds of dog I\'ve had in my entire life?').toLowerCase();
-        for (var i = 0; i < dogBreeds.length; i++) {
-            if (answerSeven === dogBreeds[i]) {
-                alert('Where my dogs at!? Argghhh, Arghhh! You got it! I\'ve had Chihuahuas, a German Shepherd, and a Rottweiler!');
-                correctAnswers++
-                console.log('first if statement');
-                console.log(dogBreeds[i]);
-                break;
-            } else if (answerSeven !== dogBreeds[i]) {
-                alert('Tail is wagging, but try again!');
-                console.log('first else if statement');
-                console.log(dogBreeds[i]);
-            } else if (num === 0) {
-                alert('Thanks for playing.. you still get a tail wag! I\'ve had Chihuahuas, a German Shepherd, and a Rottweiler');
-                console.log('second else if statement');
-            }
-        }
-        num--;
+function questionCountSeven(){
+  var dogBreeds = ['Chihuahua','German Shepherd','Rottweiler'];
+  var answerSeven;
+  var num = 6;
+  while (num > 0) {
+    answerSeven = prompt('Can you guess any of the different breeds of dog I\'ve had in my entire life?').toLowerCase();
+    for (var i = 0; i < dogBreeds.length; i++) {
+      if (answerSeven === dogBreeds[i]) {
+        alert('Where my dogs at!? Argghhh, Arghhh! You got it! I\'ve had Chihuahuas, a German Shepherd, and a Rottweiler!');
+        correctAnswers++;
+        console.log('first if statement');
+        console.log(dogBreeds[i]);
+        break;
+      } else if (answerSeven !== dogBreeds[i]) {
+        alert('Tail is wagging, but try again!');
+        console.log('first else if statement');
+        console.log(dogBreeds[i]);
+      } else if (num === 0) {
+        alert('Thanks for playing.. you still get a tail wag! I\'ve had Chihuahuas, a German Shepherd, and a Rottweiler');
+        console.log('second else if statement');
+      }
     }
+    num--;
+  }
+}
+questionCountSeven();
